bd6b45593a756fb185fdad0c4b614f417303cfa5
acostajluism <acostajluism@gmail.com>
2022-11-12 16:37:01 -0300

    1

---
+++ js/simulador.js
@@ -0,0 +1,22 @@
+const arrayPeliculas = [];
+
+const pelicula1 = new pelicula(
+    1,
+    'Back to the future',
+    'La pelicula trata sobre un adolescente llamado Marty McFly que accidentalmente viajar a través del tiempo y ahora debe viajar a su época original o deja de existir para siempre',
+)
+
+const pelicula2 = new pelicula(
+    2,
+    'Back to the future 2',
+    'Pelicula 1',
+)
+
+const pelicula3 = new pelicula(
+    3,
+    'Back to the future 3',
+    'Pelicula 1',
+)
+
+arrayPeliculas.push(pelicula1,pelicula2,pelicula3);
+
